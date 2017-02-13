89ea704b53f266a8fe555b743f3ce58fa6ba31cd
Ian Price <michael.ian.price@gmail.com>
2017-02-13 12:25:40 -0500

    Almost done with Exercise 4, the second "be" isn't getting counted

---
+++ dictionaries/4.py
@@ -0,0 +1,27 @@
+def isLetter(i):
+    letterstring = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if i in letterstring:
+        return True
+    else:
+        return False
+
+def word_histogram(p):
+    h = {}
+    n = ""
+    for char in p:
+        if isLetter(char):
+            n += char.lower()
+        else:
+            if len(n) > 0:
+                h[n] = h.get(n,0) + 1
+                print n
+                print h[n]
+                n = ""
+    return h
+
+def printHistogram(i):
+    for key,value in i.items():
+        print "%s :: %d"%(key,value)
+
+x = word_histogram('To be or not to be')
+printHistogram(x)
