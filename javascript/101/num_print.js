815981808231a7929feea8daddf2e7996d64c8ea
Ian Price <michael.ian.price@gmail.com>
2017-03-01 11:28:22 -0500

    Print numbers exercise complete

---
+++ javascript/101/num_print.js
@@ -0,0 +1,17 @@
+//FOR LOOP
+function printNumbers(start,end) {
+  for (start;start<=end;start++) {
+    console.log(start);
+  }
+}
+
+
+//WHILE LOOP
+// function printNumbers(start,end) {
+//   while (start <= end) {
+//     console.log(start);
+//     start++;
+//   }
+// }
+
+printNumbers(1,10);
