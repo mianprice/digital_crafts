a7b1f0f79044ca4fbf0676ca0ddfea81c768008e
Ian Price <michael.ian.price@gmail.com>
2017-03-07 14:12:05 -0500

    AI implemented, will try to block winning move before using random move.

--- front-end/jquery/tictactoe/ttt.js
+++ front-end/jquery/tictactoe/ttt.js
@@ -1,6 +1,82 @@
 var player1 = true;
 var winner = '';
 var counter = [0,0];
+var possible = ["one","two","three","four","five","six","seven","eight","nine"];
+var cpuThinking = false;
+
+function indexToID(ind) {
+  switch (ind) {
+    case 0:
+      return "one";
+    case 1:
+      return "two";
+    case 2:
+      return "three";
+    case 3:
+      return "four";
+    case 4:
+      return "five";
+    case 5:
+      return "six";
+    case 6:
+      return "seven";
+    case 7:
+      return "eight";
+    case 8:
+      return "nine";
+  }
+}
+
+function cpuMove() {
+  cpuThinking = true;
+  var status = $(".status").text();
+  $(".status").text("CPU is thinking...");
+  setTimeout(function() {
+    var changed = false;
+    var move = "";
+    var ran = true;
+    var arr = getGrid();
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].length === 0) {
+        var new_arr = JSON.parse(JSON.stringify(arr));
+        new_arr[i] = "X";
+        var must = checkWin(new_arr,false);
+        if (must) {
+          move = indexToID(i);
+          ran = false;
+          break;
+        }
+      }
+    }
+    if (!ran) {
+      $("#"+move).text("O");
+      if (checkWin(arr,true)) {
+        displayWinner();
+      }
+    } else {
+      while (!changed) {
+        move = possible[getRandomInt(0,8)];
+        var t = $("#"+move).text();
+        if (t.length === 0) {
+          t = "O";
+          changed = true;
+        }
+        $("#"+move).text(t);
+        if (checkWin(arr,true)) {
+          displayWinner();
+        }
+      }
+    }
+    cpuThinking = false;
+    $(".status").text(status);
+  },1000);
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 function reset() {
   player1 = true;
@@ -8,56 +84,67 @@ function reset() {
   $(".winner").toggle();
   $(".cell").empty();
   $(".board").on('click','.cell', function() {
+    if (cpuThinking) {
+      return;
+    }
     var t = $(this).text();
-    if (t.length === 0 && player1) {
+    if (t === "O") {
+      return;
+    }
+    if (t.length === 0) {
       t = "X";
-      player1 = false;
-    } else if (t.length === 0) {
-      t = "O";
-      player1 = true;
     }
     $(this).text(t);
-    checkWin();
+    var arr = getGrid();
+    if (checkWin(arr, true)) {
+      displayWinner();
+    } else {
+      cpuMove();
+    }
   });
 }
 
-function checkWin() {
-  var one = $("#one").text();
-  var two = $("#two").text();
-  var three = $("#three").text();
-  var four = $("#four").text();
-  var five = $("#five").text();
-  var six = $("#six").text();
-  var seven = $("#seven").text();
-  var eight = $("#eight").text();
-  var nine = $("#nine").text();
+function getGrid() {
+  var arr = $(".cell").map(function() {
+    return $(this).text();
+  });
+  return arr;
+}
+
+function checkWin(arr, bool) {
   if (
-    (four === five && five === six) ||
-    (two === five && five === eight) ||
-    (one === five && five === nine) ||
-    (three === five && five === seven)
+    (arr[3] === arr[4] && arr[4] === arr[5]) ||
+    (arr[1] === arr[4] && arr[4] === arr[7]) ||
+    (arr[0] === arr[4] && arr[4] === arr[8]) ||
+    (arr[2] === arr[4] && arr[4] === arr[6])
   ) {
-    winner = five;
+    winner = arr[4];
   } else if (
-    (one === two && two === three) ||
-    (one === four && four === seven)
+    (arr[0] === arr[1] && arr[1] === arr[2]) ||
+    (arr[0] === arr[3] && arr[3] === arr[6])
   ) {
-    winner = one;
+    winner = arr[0];
   } else if (
-    (three === six && six === nine) ||
-    (seven === eight && eight === nine)
+    (arr[2] === arr[5] && arr[5] === arr[8]) ||
+    (arr[6] === arr[7] && arr[7] === arr[8])
   ) {
-    winner = nine;
+    winner = arr[8];
+  }
+  if (!bool) {
+
   }
   if (winner.length === 1) {
-    displayWinner();
+    if (!bool) {
+      winner = "";
+    }
+    return true;
   }
 }
 
 function displayWinner() {
-  var winString = winner === 'X' ? "Player 1 is the winner!" : "Player 2 is the winner!";
+  var winString = winner === 'X' ? "You win!" : "CPU wins!";
   updateCounter(winner);
-  var playButton = "<div class='play-again'>Play Again!</div>";
+  var playButton = "<div class='play-again'>Play Again</div>";
   $(".winner").text(winString);
   $(".winner").append(playButton);
   $(".winner").toggle();
