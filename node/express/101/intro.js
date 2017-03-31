4a140f5fd4ffd45abc3bbe811cc222f1f95382ea
Ian Price <michael.ian.price@gmail.com>
2017-03-31 17:30:37 -0400

    Express w/db interaction complete

--- node/express/101/intro.js
+++ node/express/101/intro.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var pgp = require('pg-promise')();
 
 var app = express();
 
@@ -52,6 +53,43 @@ app.get('/fav_animals', function(req, res) {
   res.render('fav_animals.hbs', {
     animals: animalsArray
   });
+});
+
+app.get('/music', function(req, res) {
+  var db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'music_db'
+  });
+  db.any('select distinct on(sub."TrackID") sub."Song", sub."Length", sub."Artist", sub."Album", sub."Year" from (select track.name as "Song", track.duration as "Length", artist.name as "Artist", album.name as "Album", album.year as "Year", track.id as "TrackID" from artist inner join artist_album on artist.id=artist_album.artist_id inner join album on artist_album.album_id=album.id inner join track on album.id=track.album_id order by artist.name, album.name, track.name)sub')
+    .then(function(data) {
+      var keys = Object.keys(data[0]);
+      var result = [];
+      data.forEach(function(element) {
+        var arr = [];
+        keys.forEach(function(k) {
+          if (k === "Length") {
+            var m = (element[k] % 60).toString();
+            if (element[k] % 60 < 10) {
+              m = "0" + m;
+            }
+            var h = ((element[k] - (element[k] % 60)) / 60).toString();
+            arr.push(h + ":" + m);
+          } else {
+            arr.push(element[k]);
+          }
+        });
+        result.push(arr);
+      });
+      res.render('music.hbs', {
+        keys: keys,
+        lines: result
+      });
+      db.end();
+    })
+    .catch(function(error) {
+      throw error;
+    });
 });
 
 // TURN ON SERVER
