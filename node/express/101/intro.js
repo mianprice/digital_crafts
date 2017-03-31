40087dfb6249ef717a814e6ab7a1848c67eb5c8a
Ian Price <michael.ian.price@gmail.com>
2017-03-31 13:44:42 -0400

    Basic Route exercise complete

--- node/express/101/intro.js
+++ node/express/101/intro.js
@@ -6,6 +6,18 @@ app.get('/', function(req, res) {
   res.send("Hello World!");
 });
 
+app.get('/cats', function(req, res) {
+  res.send("Meow");
+});
+
+app.get('/dogs', function(req, res) {
+  res.send("Woof");
+});
+
+app.get('/cats_and_dogs', function(req, res) {
+  res.send("Living together");
+});
+
 app.listen(3000, function() {
   console.log('ex.js listening on port 3000');
 });
