6afeb277fea7f621e7e590585eb0d80ad91f091f
Ian Price <michael.ian.price@gmail.com>
2017-03-31 13:47:30 -0400

    Route parameters ex. complete

--- node/express/101/intro.js
+++ node/express/101/intro.js
@@ -2,10 +2,12 @@ var express = require('express');
 
 var app = express();
 
+// BASIC
 app.get('/', function(req, res) {
   res.send("Hello World!");
 });
 
+// ROUTES
 app.get('/cats', function(req, res) {
   res.send("Meow");
 });
@@ -18,6 +20,12 @@ app.get('/cats_and_dogs', function(req, res) {
   res.send("Living together");
 });
 
+// ROUTE PARAMETERS
+app.get('/greet/:name', function(req, res) {
+  res.send("Hello " + req.params.name);
+});
+
+// TURN ON SERVER
 app.listen(3000, function() {
   console.log('ex.js listening on port 3000');
 });
