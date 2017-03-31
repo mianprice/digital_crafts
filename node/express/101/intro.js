598e60e75ace6f6e9984a54743e1760f53188c9c
Ian Price <michael.ian.price@gmail.com>
2017-03-31 13:58:43 -0400

    Template exercise 1 complete

--- node/express/101/intro.js
+++ node/express/101/intro.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'hbs');
+
 // BASIC
 app.get('/', function(req, res) {
   res.send("Hello World!");
@@ -22,7 +24,14 @@ app.get('/cats_and_dogs', function(req, res) {
 
 // ROUTE PARAMETERS
 app.get('/greet/:name', function(req, res) {
-  res.send("Hello " + req.params.name);
+  if (req.query.age !== undefined) {
+    res.render('greet.hbs', {
+      name: req.params.name,
+      year: 2017 - req.query.age
+    });
+  } else {
+    res.send("Hello " + req.params.name);
+  }
 });
 
 // QUERY PARAMETERS
