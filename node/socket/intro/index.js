852ef46f1ffb4a554b612e19250159c97c0decf4
Ian Price <michael.ian.price@gmail.com>
2017-04-06 15:16:46 -0400

    Basic socket.io tutorial complete

--- node/socket/intro/index.js
+++ node/socket/intro/index.js
@@ -1,8 +1,25 @@
-const app = require('express');
+const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req,res) => {
-  res.send('Hello World');
+  res.sendFile(__dirname + '/app.html');
+});
+
+io.on('connection', (socket) => {
+  var connection_msg = `A user connected. Socket ID: ${socket.id}`;
+  console.log(connection_msg);
+  io.emit('message', connection_msg);
+  socket.on('disconnect', () => {
+    var disconnect_msg = `A user disconnected. Socket ID ${socket.id}`;
+    console.log(connection_msg);
+    io.emit('message', disconnect_msg);
+  });
+  socket.on('message', (msg) => {
+    var message = `${socket.id}:: ${msg}`;
+    console.log(message);
+    socket.broadcast.emit('message', message);
+  });
 });
 
 http.listen(3000, () => {
