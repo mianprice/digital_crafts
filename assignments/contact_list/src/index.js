40a897bcef9c779185982b2c61df8ef3bb8e9cfb
Ian Price <michael.ian.price@gmail.com>
2017-04-27 11:25:57 -0400

    all tasks complete using 1 component

--- assignments/contact_list/src/index.js
+++ assignments/contact_list/src/index.js
@@ -14,23 +14,52 @@ class Contact {
 }
 
 let contact_list = [];
-contact_list.push(new Contact('Alice','555-555-5555','alice@fake.com','test',false));
-contact_list.push(new Contact('Bob','555-555-5555','alice@fake.com','test',false));
-contact_list.push(new Contact('Carol','555-555-5555','alice@fake.com','test',true));
-contact_list.push(new Contact('Dan','555-555-5555','alice@fake.com','test',false));
+contact_list.push(new Contact('Alice','555-555-5555','alice@fake.com','none',false));
+contact_list.push(new Contact('Bob','555-555-5555','alice@fake.com','family',false));
+contact_list.push(new Contact('Carol','555-555-5555','alice@fake.com','friend',true));
+contact_list.push(new Contact('Dan','555-555-5555','alice@fake.com','coworker',false));
+
+// class AddContact extends React.Component {
+//   constructor(props) {
+//     super();
+//     this.state = props.state;
+//   }
+//   render() {
+//     return (
+//       <div className={this.state.covered ? "add_contact" : "noshow"}>
+//         <div className="add_title"><div className="text_contain emph_text">Add New Contact</div></div>
+//         <div><div className="text_contain">Name: </div></div>
+//         <input className="form-control" type="text" value={this.state.name} onChange={event => {this.makeStateChange('name',event)}}/>
+//         <div><div className="text_contain">Phone: </div></div>
+//         <input className="form-control" type="tel" value={this.state.phone} onChange={event => {this.makeStateChange('phone',event)}}/>
+//         <div><div className="text_contain">Email: </div></div>
+//         <input className="form-control" type="email" value={this.state.email} onChange={event => {this.makeStateChange('email',event)}}/>
+//         <div><div className="text_contain">Type: </div></div>
+//         <select className="form-control" onChange={event => {this.makeStateChange('type',event)}}>
+//           <option value="none">None</option>
+//           <option value="family">Family</option>
+//           <option value="friend">Friend</option>
+//           <option value="coworker">Coworker</option>
+//         </select>
+//         <div id="creator_container"><div className="create_contact add_control"><div className="text_contain" onClick={event => {this.addContact()}}>Create Contact</div></div></div>
+//       </div>
+//     )
+//   }
+// }
 
 class App extends React.Component {
   constructor(props) {
     super();
     this.state = {
       contacts: props.contactList,
-      contact_ind: 0,
-      name: undefined,
-      phone: undefined,
-      email: undefined,
+      edit_index: undefined,
+      name: '',
+      phone: '',
+      email: '',
       type: 'none',
       fave: false,
-      covered: false
+      covered: false,
+      fave_filter: false
     };
   }
   makeStateChange(str,event) {
@@ -43,39 +72,113 @@ class App extends React.Component {
       covered: bool
     });
   }
+  addContact() {
+    let c = this.state.contacts;
+    if (this.state.edit_index !== undefined) {
+      let current = c[this.state.edit_index];
+      current.name = this.state.name;
+      current.phone = this.state.phone;
+      current.email = this.state.email;
+      current.type = this.state.type;
+      c[this.state.edit_index] = current;
+    } else {
+      c.push(new Contact(this.state.name,this.state.phone,this.state.email,this.state.type,false));
+    }
+    this.setState({
+      contacts: c,
+      edit_index: undefined,
+      name: '',
+      phone: '',
+      email: '',
+      type: 'none',
+      fave: false,
+      covered: false
+    });
+  }
+  editContact(idx) {
+    let current = this.state;
+    let editing = this.state.contacts[idx];
+    console.log(editing);
+    current.name = editing.name;
+    current.phone = editing.phone;
+    current.email = editing.email;
+    current.type = editing.type;
+    current.covered = true;
+    current.edit_index = idx;
+    this.setState(current);
+  }
+  deleteContact(idx) {
+    let c = this.state.contacts;
+    c.splice(idx,1);
+    this.setState({
+      contacts: c
+    });
+  }
+  toggleFave(idx) {
+    let c = this.state.contacts;
+    c[idx].fave = !c[idx].fave;
+    this.setState({
+      contacts: c
+    });
+  }
+  toggleFaveFilter() {
+    this.setState({
+      fave_filter: !this.state.fave_filter
+    });
+  }
   render() {
-    console.log(this.state.name);
-    console.log(this.state.phone);
-    console.log(this.state.email);
-    console.log(this.state.type);
-    console.log(this.state.fave);
+    let current_contacts = this.state.contacts.map((element,idx) => {
+      element.idx = idx;
+      return element;
+    });
+    if (this.state.fave_filter) {
+      current_contacts = current_contacts.filter((element) => {
+        return element.fave;
+      });
+    }
     return (
       <div className="app">
-        <div className={this.state.covered ? "add_contact" : "noshow"}>
-          <div className="add_title"><div className="text_contain emph_text">Add New Contact</div></div>
-          <div><div className="text_contain">Name: </div></div>
-          <input className="form-control" type="text" value={this.state.name} onChange={event => {this.makeStateChange('name',event)}}/>
-          <div><div className="text_contain">Phone: </div></div>
-          <input className="form-control" type="tel" value={this.state.phone} onChange={event => {this.makeStateChange('phone',event)}}/>
-          <div><div className="text_contain">Email: </div></div>
-          <input className="form-control" type="email" value={this.state.email} onChange={event => {this.makeStateChange('email',event)}}/>
-          <div><div className="text_contain">Type: </div></div>
-          <select className="form-control" onChange={event => {this.makeStateChange('type',event)}}>
-            <option value="none">None</option>
-            <option value="family">Family</option>
-            <option value="friend">Friend</option>
-            <option value="coworker">Coworker</option>
-          </select>
-          <div id="creator_container"><div className="create_contact"><div className="text_contain">Create Contact</div></div></div>
+        <div className={this.state.covered ? "add_contact_wrap" : "noshow"} onClick={event => {this.covered(false)}}>
+          <div className={this.state.covered ? "add_contact" : "noshow"}>
+            <div className="add_title"><div className="text_contain emph_text">Add New Contact</div></div>
+            <div><div className="text_contain">Name: </div></div>
+            <input className="form-control" type="text" value={this.state.name} onChange={event => {this.makeStateChange('name',event)}}/>
+            <div><div className="text_contain">Phone: </div></div>
+            <input className="form-control" type="tel" value={this.state.phone} onChange={event => {this.makeStateChange('phone',event)}}/>
+            <div><div className="text_contain">Email: </div></div>
+            <input className="form-control" type="email" value={this.state.email} onChange={event => {this.makeStateChange('email',event)}}/>
+            <div><div className="text_contain">Type: </div></div>
+            <select className="form-control" onChange={event => {this.makeStateChange('type',event)}}>
+              <option value="none">None</option>
+              <option value="family">Family</option>
+              <option value="friend">Friend</option>
+              <option value="coworker">Coworker</option>
+            </select>
+            <div id="creator_container">
+              <div className="create_contact add_control">
+                <div className="text_contain" onClick={event => {this.addContact()}}>Create Contact</div>
+              </div>
+              <div className="go_back add_control">
+                <div className="text_contain" onClick={event => {this.covered(false)}}>Go Back</div>
+                </div>
+              </div>
+          </div>
         </div>
         <div className="contact_list">
-          <div className="list_title"><div className="text_contain emph_text" onClick={event => {this.covered(true)}}>Contacts</div></div>
-          {this.state.contacts.map((element,idx) => (
-            <div key={idx} className="contact">
+          <div className="list_title">
+            <div className="text_contain emph_text">Contacts</div>
+            <div className="app_controls">
+              <div className="text_contain app_control" onClick={event => {this.covered(true)}}>Add Contact</div>
+              <div className="text_contain app_control" onClick={event => {this.toggleFaveFilter()}}>{this.state.fave_filter ? "Show All" : "Show Favorites"}</div>
+              <div className="text_contain app_control">Menu</div>
+            </div>
+          </div>
+          {current_contacts.map((element) => (
+            <div key={element.idx} className="contact">
               <div className="c_name"><div className="text_contain">{element.name}</div><div className="text_contain c_type">{element.type}</div></div>
               <div className="c_phone"><div className="text_contain"><i className="fa fa-fw fa-phone-square"></i>{element.phone}</div></div>
               <div className="c_email"><div className="text_contain"><i className="fa fa-fw fa-envelope"></i>{element.email}</div></div>
-              <div className="controls"><div className="text_contain favorite"><i className={element.fave ? "fa fa-fw fa-star" : "fa fa-fw fa-star-o"}></i></div><div className="text_contain edit">Edit</div><div className="text_contain delete">Delete</div></div>
+              <div className="controls"><div className="text_contain favorite contact_control"><i className={element.fave ? "fa fa-fw fa-star" : "fa fa-fw fa-star-o"} onClick={event => {this.toggleFave(element.idx)}}></i></div><div className="text_contain edit contact_control" onClick={event => {this.editContact(element.idx)}}>Edit</div><div className="text_contain delete contact_control" onClick={event => {this.deleteContact(element.idx)}}>Delete</div></div>
             </div>
           ))}
         </div>
