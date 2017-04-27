6cd9a9c7289afcab3caac41c45c155c7eebf754a
Ian Price <michael.ian.price@gmail.com>
2017-04-26 22:27:03 -0400

    styling changed, state changes mostly handled for contact form

--- assignments/contact_list/src/index.js
+++ assignments/contact_list/src/index.js
@@ -24,28 +24,56 @@ class App extends React.Component {
     super();
     this.state = {
       contacts: props.contactList,
-      current_contact: {
-        name: undefined,
-        phone: undefined,
-        email: undefined,
-        type: undefined
-      }
+      contact_ind: 0,
+      name: undefined,
+      phone: undefined,
+      email: undefined,
+      type: 'none',
+      fave: false,
+      covered: false
     };
   }
+  makeStateChange(str,event) {
+    let x = this.state;
+    if (str === 'fave') {
+      x[str] = event.target.checked;
+    } else {
+      x[str] = event.target.value;
+    }
+    this.setState(x);
+  }
+  covered(bool) {
+    this.setState({
+      covered: bool
+    });
+  }
   render() {
+    console.log(this.state.name);
+    console.log(this.state.phone);
+    console.log(this.state.email);
+    console.log(this.state.type);
+    console.log(this.state.fave);
     return (
       <div className="app">
-        <div className="add_contact">
+        <div className={this.state.covered ? "add_contact" : "noshow"}>
           <div className="add_title"><div className="text_contain emph_text">Add New Contact</div></div>
-          <div><div className="text_contain">Name</div></div><input className="form-control" type="text"/>
-          <div><div className="text_contain">Phone</div></div><input className="form-control" type="text"/>
-          <div><div className="text_contain">Email</div></div><input className="form-control" type="text"/>
-          <div><div className="text_contain">Type</div></div><input className="form-control" type="text"/>
-          // Family, Friend, Coworker, Other \\
-
+          <div><div className="text_contain">Name: </div></div>
+          <input className="form-control" type="text" value={this.state.name} onChange={event => {this.makeStateChange('name',event)}}/>
+          <div><div className="text_contain">Phone: </div></div>
+          <input className="form-control" type="tel" value={this.state.phone} onChange={event => {this.makeStateChange('phone',event)}}/>
+          <div><div className="text_contain">Email: </div></div>
+          <input className="form-control" type="email" value={this.state.email} onChange={event => {this.makeStateChange('email',event)}}/>
+          <div><div className="text_contain">Type: </div></div>
+          <select className="form-control" onChange={event => {this.makeStateChange('type',event)}}>
+            <option value="none">None</option>
+            <option value="family">Family</option>
+            <option value="friend">Friend</option>
+            <option value="coworker">Coworker</option>
+          </select>
+          <div id="creator_container"><div className="create_contact"><div className="text_contain">Create Contact</div></div></div>
         </div>
         <div className="contact_list">
-          <div className="list_title"><div className="text_contain emph_text">Contacts</div></div>
+          <div className="list_title"><div className="text_contain emph_text" onClick={event => {this.covered(true)}}>Contacts</div></div>
           {this.state.contacts.map((element,idx) => (
             <div key={idx} className="contact">
               <div className="c_name"><div className="text_contain">{element.name}</div><div className="text_contain c_type">{element.type}</div></div>
@@ -55,6 +83,7 @@ class App extends React.Component {
             </div>
           ))}
         </div>
+        <div className={this.state.covered ? "app_cover" : ""} onClick={event => {this.covered(false)}}></div>
       </div>
     );
   }
